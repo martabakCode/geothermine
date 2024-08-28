f139f4308380a14316d8ebabfe044db2909b7b1e
martbakCode <rajendranohan4@gmail.com>
2024-08-28 20:58:38 +0700

    Fix '

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,19 @@
-// pages/_document.tsx
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Ensure there are no conflicting favicon links here */}
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
