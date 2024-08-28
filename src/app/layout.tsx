f7354ba6ffda3b13ac8aa9a40d6b16adc0ee816f
martbakCode <rajendranohan4@gmail.com>
2024-08-28 21:47:55 +0700

    Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
+// app/layout.tsx or src/app/layout.tsx
 import './globals.css';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 import Head from 'next/head';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Geothermine',
+  description: 'Geothermine is pioneering a sustainable future by merging geothermal energy with cutting-edge crypto mining technology.',
+};
 
 export default function RootLayout({
   children,
@@ -15,12 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <Head>
-        <link rel="icon" href={'/favicon.ico'} type="image/x-icon" />
-        <title>Geothermine</title>
-        <meta
-          name="description"
-          content="Geothermine is pioneering a sustainable future by merging geothermal energy with cutting-edge crypto mining technology."
-        />
+        <link rel="icon" href="/logo/favicon.ico" type="image/x-icon" sizes="256x256" />
       </Head>
       <body className={inter.className}>
         <Header />
