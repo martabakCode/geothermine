54735ece72116405c67d2a577b1e37dbb86f9dae
martbakCode <rajendranohan4@gmail.com>
2024-08-28 20:46:45 +0700

    Icon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Ensure there are no conflicting favicon links here */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
